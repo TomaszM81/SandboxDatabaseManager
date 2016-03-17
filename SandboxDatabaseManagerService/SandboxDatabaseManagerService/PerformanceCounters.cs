f761ca288c8ac151aaeaebdb4ed5719c6e41d77d
Tomasz Moska <tmoska@gmail.com>
2016-03-17 09:44:30 +0100

    Update PerformanceCounters.cs

--- SandboxDatabaseManagerService/SandboxDatabaseManagerService/PerformanceCounters.cs
+++ SandboxDatabaseManagerService/SandboxDatabaseManagerService/PerformanceCounters.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Data;
 using System.Data.SqlClient;
@@ -20,6 +20,7 @@ namespace SandboxDatabaseManagerService
         List<Tuple<string, string, PerformanceCounter, int?, string, int?, int?>> performanceCounters = new List<Tuple<string, string, PerformanceCounter, int?, string, int?, int?>>();
         private PerformanceCounterResults _result = new PerformanceCounterResults();
         private List<MonitoredFSRMQuota> MonitoredFSRMQuotaList = new List<MonitoredFSRMQuota>();
+        List<Tuple<string, string, System.IO.DriveInfo, int?, string, int?, int?>> driveInfoCounters = new List<Tuple<string, string, System.IO.DriveInfo, int?, string, int?, int?>>();
         private IFsrmQuotaManager FsrmQuotaManager = null;
         private object _sync = new object();
         private Task _worker;
@@ -53,6 +54,37 @@ namespace SandboxDatabaseManagerService
             {
                 try
                 {
+                    // Special case for reporting LogicalDisk\Free MegaBytes counter values because the counter does only update once per 5 minutes, need to make this more frequent by using System.IO.DriveInfo from .Net
+                    if (String.Compare("LogicalDisk", item.CategoryName, true) == 0 && String.Compare("Free MegaBytes", item.CounterName, true) == 0 && item.InstanceName.Length > 0)
+                    {
+                        bool found = false;
+                        foreach(var driveInfo in System.IO.DriveInfo.GetDrives())
+                        {
+                            if(String.Compare(driveInfo.Name.Substring(0,1), item.InstanceName.Substring(0, 1), true) == 0)
+                            {
+                                found = true;
+
+                                driveInfoCounters.Add(new Tuple<string, string, System.IO.DriveInfo, int?, string, int?, int?>(item.FriendlyName,
+                                                        item.DotNetFormatString,
+                                                        driveInfo,
+                                                        String.IsNullOrWhiteSpace(item.DivideRawCounterValueBy) ? null : (int?)Int32.Parse(item.DivideRawCounterValueBy),
+                                                        item.ChartYAxisSufix,
+                                                        String.IsNullOrWhiteSpace(item.LowWarningValue) ? null : (int?)Int32.Parse(item.LowWarningValue),
+                                                        String.IsNullOrWhiteSpace(item.HighWarningValue) ? null : (int?)Int32.Parse(item.HighWarningValue)
+                                                        ));
+
+                            }
+                        }
+
+                        if(!found)
+                        {
+                            Log.Error(String.Format("Failed initialize counter (System.IO.DriveInfo), Category: {0}, Counter:{1}, Instance:{2}. This counter will not be reported.", item.CategoryName, item.CounterName, item.InstanceName));
+                        }
+
+                        continue;
+                    }
+
+
                     PerformanceCounter counter = new PerformanceCounter
                     {
                         CategoryName = item.CategoryName,
@@ -202,8 +234,38 @@ namespace SandboxDatabaseManagerService
                         }
                     }
 
+                    foreach (var value in PerformanceCounters.Instance.driveInfoCounters)
+                    {
+                        try
+                        {
 
-                    if(FsrmQuotaManager != null)
+
+                            float counterValueRaw = value.Item3.TotalFreeSpace / 1048576;
+                            
+                            bool warning = false;
+                            
+                            if (value.Item4.HasValue)
+                                counterValueRaw = counterValueRaw / value.Item4.Value;
+
+                            if (value.Item6.HasValue && counterValueRaw < value.Item6.Value)
+                                warning = true;
+
+                            if (value.Item7.HasValue && counterValueRaw > value.Item7.Value)
+                                warning = true;
+
+
+                            resultNew.ResultList.Add(new PerformanceCounterResults.PerformanceCounterResult() { CounterFriendlyName = value.Item1, CounteValue = counterValueRaw, DotNetFormatString = value.Item2, ChartYAxisSufix = value.Item5, Warning = warning });
+                      
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(String.Format("Failed to retrieve result from performance counter (by driveInfo), counter FriendlyNamme: {0}", value.Item1), ex);
+                        }
+                    }
+
+
+                    if (FsrmQuotaManager != null)
                     {
                         foreach(var quotaItem in MonitoredFSRMQuotaList)
                         {
